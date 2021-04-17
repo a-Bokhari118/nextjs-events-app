376ceb29e13deb6c9a5357d15c95cd6e9c009cc8
Abdulrhman Bokhari <a.bokhari118@hotmail.com>
2021-04-18 00:37:07 +0300

    first commit

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h2>Heyyyyy, Where a u Going !!!</h2>
+      <h3>This Page Does Not Exists!</h3>
+    </div>
+  );
+};
+
+export default NotFoundPage;
