c71cf8beb44966458857fed9c90f3846c2370e0e
Abdulrhman Bokhari <a.bokhari118@hotmail.com>
2021-04-18 02:21:35 +0300

    implemented home page p-1

---
+++ components/events/EventItem.js
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import classes from './EventItem.module.css';
+const EventItem = ({ event: { title, image, date, location, id } }) => {
+  const hdate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+
+  const daddress = location.replace(', ', '\n');
+  const exploreLink = `events/${id}`;
+  return (
+    <li className={classes.item}>
+      <img src={`/${image}`} alt={title} />
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <time>{hdate}</time>
+          </div>
+          <div className={classes.address}>
+            <address>{daddress}</address>
+          </div>
+        </div>
+        <div className={classes.actions}>
+          <Link href={exploreLink}>Explore Event</Link>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default EventItem;
---
+++ pages/events/[...slug].js
@@ -0,0 +1,9 @@
+const FilteredEvenstPage = () => {
+  return (
+    <div>
+      <h1>Filterd Events</h1>
+    </div>
+  );
+};
+
+export default FilteredEvenstPage;
